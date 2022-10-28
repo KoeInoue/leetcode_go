22d37dd26dd289b94263b43848d057401d05bea5
KoeInoue <kjustininoue1996@gmail.com>
2022-10-28 22:52:16 +0900

    Closes #10 remove duplicate

--- problems/26_remove_duplicates_from_sorted_array.go
+++ problems/26_remove_duplicates_from_sorted_array.go
@@ -1,5 +1,15 @@
 package problem
 
 func removeDuplicates(nums []int) int {
-	return 2
+	a := 0
+	if len(nums) == 0 {
+		return a
+	}
+	for b := 1; b < len(nums); b++ {
+		if nums[a] != nums[b] {
+			a++
+			nums[a] = nums[b]
+		}
+	}
+	return a + 1
 }
--- problems/26_remove_duplicates_from_sorted_array_test.go
+++ problems/26_remove_duplicates_from_sorted_array_test.go
@@ -5,12 +5,28 @@ import (
 )
 
 func TestRemoveDuplicates(t *testing.T) {
-	t.Run("Test with array {1,1,2}", func(t *testing.T) {
-		given := []int{1, 1, 2}
-		want := []int{1, 2}
+	cases := []struct {
+		Name   string
+		Given  []int
+		Expect []int
+	}{
+		{
+			"Test with array {1,1,2}",
+			[]int{1, 1, 2},
+			[]int{1, 2},
+		},
+		{
+			"Test with array {1,2,2, 3, 4, 4, 5,5}",
+			[]int{1, 2, 2, 3, 4, 4, 5, 5},
+			[]int{1, 2, 3, 4, 5},
+		},
+	}
 
-		assertInt(t, want, given)
-	})
+	for _, test := range cases {
+		t.Run(test.Name+" length", func(t *testing.T) {
+			assertInt(t, test.Expect, test.Given)
+		})
+	}
 }
 
 func assertInt(t testing.TB, want, given []int) {
